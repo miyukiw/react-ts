ff7a0ecaf5b8b03c6fdc1f89d763f99441def1dc
Miyuki Watanabe <miyuki.wtnb@gmail.com>
2019-08-25 23:30:34 +0900

    Add redux, router, tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router'; // react-router v4/v5
+import { ConnectedRouter } from 'connected-react-router';
 import './index.css';
-import App from './App';
+import configureStore, { history } from './redux/configureStore';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore({});
+
+const renderApp = () => (<App />);
+const renderNotFound = () => (<div>Not Found</div>);
+
+const dom = (
+  <Provider store={store}>
+      <ConnectedRouter history={history}> {/* place ConnectedRouter under Provider */}
+      <> {/* your usual react-router v4/v5 routing */}
+          <Switch>
+              <Route exact={true} path="/" render={renderApp} />
+              <Route render={renderNotFound} />
+          </Switch>
+      </>
+      </ConnectedRouter>
+  </Provider>
+);
+
+ReactDOM.render(
+  dom,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
