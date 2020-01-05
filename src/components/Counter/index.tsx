fa548d8289b46425c0853bf486315c85f3e26acd
Miyuki Watanabe <miyuki.wtnb@gmail.com>
2020-01-05 18:17:15 +0900

    Use useSelector, useDispatch

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,47 +1,31 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../redux/reducer';
 import { increment, decrement } from '../../redux/counter/actions';
-import { Dispatch } from 'redux';
 
-type StateProps = {
-  count: number;
-};
+const Counter: React.FC = () => {
+  const counter = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
 
-type DispatchProps = {
-  increment: () => void;
-  decrement: () => void;
-};
+  const handleIncrement = () => {
+    dispatch(increment(1));
+  };
 
-const Counter: React.FC<StateProps & DispatchProps> = (props) => {
+  const handleDecrement = () => {
+    dispatch(decrement(1));
+  };
+
   return (
     <div>
-      <p>{props.count}</p>
-      <button type="button" onClick={props.increment}>
+      <p>{counter.count}</p>
+      <button type="button" onClick={handleIncrement}>
         + increment
       </button>
-      <button type="button" onClick={props.decrement}>
+      <button type="button" onClick={handleDecrement}>
         + decrement
       </button>
     </div>
   );
 };
 
-const mapStateToProps = (state: RootState) => {
-  return ({
-    count: state.counter.count,
-  });
-};
-
-// "dispatch" argument needs an annotation to check the correct shape
-//  of an action object when using dispatch function
-const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
-  increment: () => {
-    dispatch(increment(1));
-  },
-  decrement: () => {
-    dispatch(decrement(1));
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
